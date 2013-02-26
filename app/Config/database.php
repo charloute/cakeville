a4bd8b716e87fd301b67d920a84557b9d11659b6
Vanessa <sarahad@Poretia.local>
2013-02-26 16:17:00 +0100

    je rajoute la configuration

---
+++ app/Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'cakephp',
+		'password' => 'cakephp',
+		'database' => 'cakeville',
+		'encoding' => 'utf8'
+	);
+}
