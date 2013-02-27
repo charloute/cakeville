35d62ba724251a326301edabfb94851310d5b34b
Charlotte Vidaud <charlottevid@hotmail.fr>
2013-02-27 15:41:31 +0100

    Upload d'image lors de l'ajout d'une nouvelle image

--- app/Model/Picture.php
+++ app/Model/Picture.php
@@ -7,12 +7,21 @@ App::uses('AppModel', 'Model');
  */
 class Picture extends AppModel {
 
+	public $useTable = 'pictures';
+	public $validate = array(
+		'url' => array(
+			'rule' => '/^.*\.(jpg|png|jpeg)$/',
+			'allowEmpty' => true,
+			'message' => 'le fichier est pas valide'
+			)
+	);
+
 /**
  * Display field
  *
  * @var string
  */
-	public $displayField = 'user_id';
+	//public $displayField = 'url';
 
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
